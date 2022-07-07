c1022ad981695c2ead9c0a1247516de9f04122b8
agence-appy <contact@agence-appy.fr>
2022-07-07 14:22:07 +0200

    fix provider

--- src/AppyProvider.php
+++ src/AppyProvider.php
@@ -13,7 +13,12 @@ class AppyProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->register('Appy\FcmHttpV1\ApyProvider');
+
+        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        $loader->alias('AppyFcmHttpV1', 'Appy\FcmHttpV1\Classes\AppyFcmHttpV1');
+        $loader->alias('AppyNotification', 'Appy\FcmHttpV1\Classes\AppyNotification');
+        $loader->alias('AppyGoogleHelper', 'Appy\FcmHttpV1\Helpers\AppyGoogleHelper');
     }
 
     /**
@@ -24,7 +29,7 @@ class AppyProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'\config\appy_firebase.php' => config_path('appy_firebase.php'),
+            __DIR__ . '\config\appy_firebase.php' => config_path('appy_firebase.php'),
         ], 'appyfcmhttpv1');
     }
 }
